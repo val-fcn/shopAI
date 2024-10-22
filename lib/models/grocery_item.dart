36e82c9ab7be1552ce46d4660093ada91be6b8fe
val-fcn <valentin.faucon@gmail.com>
2024-10-22 18:36:06 +0200

    add class item

---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,11 @@
+class GroceryItem {
+  GroceryItem({
+    required this.name,
+    required this.quantity,
+    required this.purchased,
+  });
+
+  final String name;
+  final int quantity;
+  final bool purchased;
+}
