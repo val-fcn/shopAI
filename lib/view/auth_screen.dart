7d468497543ef20c42d948ad9b39e5449124f695
val-fcn <valentin.faucon@gmail.com>
2024-10-24 22:27:02 +0200

    supabase-ui used to signin & signup

--- lib/view/auth_screen.dart
+++ lib/view/auth_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_ai/services/auth_service.dart';
 import 'package:shop_ai/services/storage_service.dart';
+import 'package:shop_ai/view/home_screen.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key, required this.storageService});
@@ -13,6 +16,7 @@ class _AuthScreenState extends State<AuthScreen> {
   final _formKey = GlobalKey<FormState>();
   final userEmailController = TextEditingController();
   final userPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,37 +29,51 @@ class _AuthScreenState extends State<AuthScreen> {
             children: [
               Card(
                 child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: userEmailController,
-                          decoration: const InputDecoration(
-                            labelText: 'Email',
+                    padding: const EdgeInsets.all(16),
+                    child: SupaEmailAuth(
+                      localization: const SupaEmailAuthLocalization(
+                        enterEmail: 'Email',
+                        enterPassword: 'Mot de passe',
+                        signIn: 'Se connecter',
+                        signUp: 'S\'inscrire',
+                        forgotPassword: 'Mot de passe oublié',
+                        haveAccount: 'Vous avez déjà un compte ?',
+                        dontHaveAccount: 'Vous n\'avez pas de compte ?',
+                      ),
+                      onSignInComplete: (response) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomeScreen(
+                              storageService: widget.storageService,
+                            ),
                           ),
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Password',
+                        );
+                      },
+                      onSignUpComplete: (response) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomeScreen(
+                              storageService: widget.storageService,
+                            ),
                           ),
-                          controller: userPasswordController,
-                        ),
-                        const SizedBox(height: 16),
-                        ElevatedButton(
-                          onPressed: () {
-                            AuthService().signUp(
-                              userEmailController.text,
-                              userPasswordController.text,
-                            );
+                        );
+                      },
+                      metadataFields: [
+                        MetaDataField(
+                          prefixIcon: const Icon(Icons.person),
+                          label: 'Prénom',
+                          key: 'username',
+                          validator: (val) {
+                            if (val == null || val.isEmpty) {
+                              return 'Please enter something';
+                            }
+                            return null;
                           },
-                          child: const Text('Submit'),
-                        )
+                        ),
                       ],
-                    ),
-                  ),
-                ),
+                    )),
               )
             ],
           ),
